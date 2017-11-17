92839f748c463bd67676db9b7e8a511fbc7258d4
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2017-11-16 23:16:33 -0500

    Initial Project Structure - Required dependencies for a React project - Webpack configuration including webpack-dev-server - Bootstrap configuration for styles - Routing using react-router

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: './bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src')
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel-loader']},
+      {test: /(\.css)$/, loaders: ['style-loader', 'css-loader']},
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
+        use: [
+          {
+            loader:  'url-loader',
+            options: {
+              limit: 100000,
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
